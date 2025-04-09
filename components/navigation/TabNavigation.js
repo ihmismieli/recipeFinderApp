7cd20bcc7abf2736c611b2e3403ed1f7bf21b250
Heidi Ahlgren <heidiahlgren@hotmail.com>
2025-04-09 11:59:43 +0300

    tab navigation changed to react native tab navigation

--- components/navigation/TabNavigation.js
+++ components/navigation/TabNavigation.js
@@ -1,50 +1,81 @@
 /* Tab navigation of the application */
-/* Docs: https://callstack.github.io/react-native-paper/docs/components/BottomNavigation/BottomNavigationBar*/
+/* Docs: 
+https://callstack.github.io/react-native-paper/docs/components/BottomNavigation/BottomNavigationBar
+https://reactnavigation.org/docs/bottom-tab-navigator/
+*/
 
-import React, { useState } from 'react'
-import { PaperProvider, BottomNavigation, Title } from 'react-native-paper';
+import { BottomNavigation } from 'react-native-paper';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { CommonActions } from '@react-navigation/native';
 import HomeScreen from '../../screens/HomeScreen';
 import SettingsScreen from '../../screens/SettingsScreen';
 import FavoritesScreen from '../../screens/FavoritesScreen';
 
 const Tab = createBottomTabNavigator();
 
-const HomeRoute = () => <HomeScreen />;
-
-const FavoriteRoute = () => <FavoritesScreen />;
-
-const SettingsRoute = () => <SettingsScreen />;
-
-
 export default function TabNavigation() {
 
-
-    const [index, setIndex] = useState(0);
-    const [routes] = useState([
-        {
-            key: 'home', title: 'Recipes', focusedIcon: 'bowl-mix', unfocusedIcon: 'bowl-mix-outline'
-        },
-        {
-            key: 'favorite', title: 'Faves', focusedIcon: 'cards-heart', unfocusedIcon: 'cards-heart-outline'
-        }, 
-        {
-            key: 'settings', title: 'Settings', focusedIcon: 'account-circle', unfocusedIcon: 'account-circle-outline'
-        }
-
-    ]);
-
-    const renderScene = BottomNavigation.SceneMap({
-        home: HomeRoute,
-        favorite: FavoriteRoute,
-        settings: SettingsRoute,
-    });
-
     return (
-        <BottomNavigation
-            navigationState={{ index, routes }}
-            onIndexChange={setIndex}
-            renderScene={renderScene}
-        />
+        <Tab.Navigator
+            screenOptions={{
+                headerShown: false,
+            }}
+            tabBar={({ navigation, state, descriptors, insets }) => (
+                <BottomNavigation.Bar
+                    navigationState={state}
+                    safeAreaInsets={insets}
+                    onTabPress={({ route }) => {
+                        navigation.dispatch({
+                            ...CommonActions.navigate(route.name, route.params),
+                            target: state.key,
+                        });
+                    }}
+                    renderIcon={({ route, focused }) => {
+                        const { options } = descriptors[route.key];
+                        if (options.tabBarIcon) {
+                            return options.tabBarIcon({ focused, size: 24 });
+                        }
+
+                        return null;
+                    }}
+                    getLabelText={({ route }) => {
+                        const { options } = descriptors[route.key];
+                        return options.tabBarLabel;
+                    }}
+                />
+            )}
+        >
+            <Tab.Screen
+                name='Home'
+                component={HomeScreen}
+                options={{
+                    tabBarLabel: 'Recipes',
+                    tabBarIcon: ({ focused, size }) => {
+                        return <Icon name={focused ? 'bowl-mix' : 'bowl-mix-outline'} size={size} />
+                    }
+                }}
+            />
+            <Tab.Screen
+                name='favorite'
+                component={FavoritesScreen}
+                options={{
+                    tabBarLabel: 'Faves',
+                    tabBarIcon: ({ focused, size }) => {
+                        return <Icon name={focused ? 'cards-heart' : 'cards-heart-outline'} size={size} />
+                    }
+                }}
+            />
+            <Tab.Screen
+                name='settings'
+                component={SettingsScreen}
+                options={{
+                    tabBarLabel: 'Settings',
+                    tabBarIcon: ({ focused, size }) => {
+                        return <Icon name={focused ? 'account-circle' : 'account-circle-outline'} size={size} />
+                    }
+                }}
+            />
+        </Tab.Navigator>
     )
 }
